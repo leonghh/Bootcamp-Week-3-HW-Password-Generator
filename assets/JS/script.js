d22d2d49346e243c2a63a71764373a7bcc59b3d9
Eugene Leong <honghao@hotmail.sg>
2019-11-27 00:48:29 +1100

    completed

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,30 +1,26 @@
-var resultEl = document.getElementById('result');
-var generateEl = document.getElementById('generate');
-var clipboard = document.getElementById('clipboard');
 
-function promptQuestion() {
-    var length = prompt("How many characters will you like your password to have?")
-//if character is not a number, alert a error
+function generatePassword(){
+
+    //set password length
+    var length = prompt("How many characters will you like your password to have?");
+    //if character is not a number, alert a error
     if (isNaN(length)) {
         alert("Please type in a number.");
-        promptQuestion();
+        return;
     }
-//if character is less than 8, prompt user that paswword is too long
-    if (length < 8) {
+    //if character is less than 8, prompt user that paswword is too long
+    while (length < 8) {
         alert("Password should have 8 or more characters.");
-        promptQuestion();
+        return;
     }
-//if character is more than 128, prompt user that passwrd is too long
+    //if character is more than 128, prompt user that passwrd is too long
     if (length > 128) {
         alert("Password cannot be that long.");
-        promptQuestion();
+        return;
     }
-}
-
-function promptSelection() {
     // Confirm whether user will like to include lowercase in their paswword
     var includeLowercase = confirm("Will you like your password to include lowercase?");
-        
+            
     // //Confirm whether user will like to include uppercase in their paswword
     var includeUppercase = confirm("Will you like your password to include uppercase?");
 
@@ -33,55 +29,56 @@ function promptSelection() {
 
     // //Confirm whether user will like to include symbols in their paswword
     var includeSymbols = confirm("Will you like your password to include symbols?");
-
+    
     //The application should validate user input and ensure that at least one character type is selected.
     if (includeLowercase == false && includeUppercase == false && includeNumbers == false && includeSymbols == false) {
         alert("Please include at least one character type");
-        promptSelection();
+        return;   
     }
-}
-
-//Once all prompts are answered, computer will generate a password
-//Set all get characters function in a object
-var getCharFunc = {
-	lower: getLowercase,
-	upper: getUppercase,
-	number: getSymbol,
-	symbol: getSymbol
-}
-
-
-
-
-//Password will be displayed on the textbox
-
-//Character type:
-
-//Lowercase characters
-function getLowercase() {
+    //possible password values
+    
     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    return lowercase[Math.floor(Math.random() * lowercase.length)];
-}
-
-//Uppercase characters
-function getUppercase() {
     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    return uppercase[Math.floor(Math.random() * uppercase.length)];
-}
-
-//Numeric characters
-function getNumber() {
     var numbers = '1234567890';
-    return numbers[Math.floor(Math.random() * numbers.length)];
+    var symbols = '!@#$%^&*(){}[]=<>/,.';
+    
+    
+    var charSet = "";
+
+    if (includeLowercase == true) {
+        charSet = charSet + lowercase
+    };
+    if (includeUppercase == true) {
+        charSet = charSet + uppercase
+    };
+    if (includeNumbers == true) {
+        charSet = charSet + numbers
+    };
+    if (includeSymbols == true) {
+        charSet = charSet + symbols
+    };
+    console.log(charSet);
+
+    var password = "";
+
+    //create for loop to choose password characters
+    for(var i = 0; i < length; i++) {
+        password = password + charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+
+    //add password to textbox/display area
+    document.getElementById("result").value = password;
+
 }
 
-//Special characters (see examples)
-function getSymbol() {
-	var symbols = '!@#$%^&*(){}[]=<>/,.'
-	return symbols[Math.floor(Math.random() * symbols.length)];
+
+//function to copy password to clipboard
+function copyPassword(){
+
+    document.getElementById("result").select();
+
+    document.execCommand("Copy");
+
+    alert("Password copied to clipboard!");
+
 }
-
-
-
-
-//User should also have the option to click a button to copy the password to their clipboard.
